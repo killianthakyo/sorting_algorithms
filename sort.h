4a07bf00acbf183c7948d3dc66a2d181e60cfcb1
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-24 07:59:57 -0800

    Create header file for Prototype of my functions

---
+++ sort.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+#endif /* MAIN_H */
