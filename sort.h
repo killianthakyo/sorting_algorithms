17ba452fe7a86271a1dbbe2a4830556ca728dac5
Killian Nthakyo <killynthakyo@gmail.com>
2023-01-24 08:36:06 -0800

    Sort header

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SORT_H
+#define SORT_H
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-#endif /* MAIN_H */
+#endif /* SORT_H */
